f2a7eefbf34df495fbd44e665be53c3d3fcf0646
loganAtAioBio <logoses@daum.net>
2023-09-01 16:58:28 +0900

    pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link href="asset/favicons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="asset/favicons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+      </Head>
       <body>
         <Main />
         <NextScript />
